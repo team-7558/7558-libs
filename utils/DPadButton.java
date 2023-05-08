cdbe322ffa6af1f9adadd7ac00599821f6cd14da
Grant Bourne <grant@rbgn.ca>
2023-05-08 10:28:28 -0400

    update utils

--- utils/DPadButton.java
+++ utils/DPadButton.java
@@ -1,51 +1,53 @@
 package frc.lib.team7558.utils;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Button;
 
-/**
- * DPadButton class to make use of the four cardinal directions on the DPad
- */
+/** DPadButton class to make use of the four cardinal directions on the DPad */
 public class DPadButton extends Button {
-    
-    XboxController m_joystick;
-    Direction m_direction;
 
-    /**
-     * New DPadButton
-     * 
-     * @param joystick - The joystick to watch
-     * @param direction - The {@link DPadButton.Direction} to watch for
-     */
-    public DPadButton(XboxController joystick, Direction direction) {
-        this.m_joystick = joystick;
-        this.m_direction = direction;
+  XboxController m_joystick;
+  Direction m_direction;
+
+  /**
+   * New DPadButton
+   *
+   * @param joystick - The joystick to watch
+   * @param direction - The {@link DPadButton.Direction} to watch for
+   */
+  public DPadButton(XboxController joystick, Direction direction) {
+    this.m_joystick = joystick;
+    this.m_direction = direction;
+  }
+
+  /**
+   * The Direction on the DPad
+   *
+   * @param UP
+   * @param RIGHT
+   * @param DOWN
+   * @param LEFT
+   */
+  public static enum Direction {
+    UP(0),
+    RIGHT(90),
+    DOWN(180),
+    LEFT(270);
+
+    int direction;
+
+    private Direction(int direction) {
+      this.direction = direction;
     }
+  }
 
-    /**
-     * The Direction on the DPad
-     * 
-     * @param UP
-     * @param RIGHT
-     * @param DOWN
-     * @param LEFT
-     */
-    public static enum Direction {
-        UP(0), RIGHT(90), DOWN(180), LEFT(270);
-
-        int direction;
-
-        private Direction(int direction) {
-            this.direction = direction;
-        }
-    }
-
-    /**
-     * @return If the DPadButton is being activated
-     */
-    public boolean get() {
-        int dPadValue = m_joystick.getPOV();
-        return (dPadValue == m_direction.direction) || (dPadValue == (m_direction.direction + 45) % 360) || (dPadValue == (m_direction.direction + 315) % 360);
-    }
+  /**
+   * @return If the DPadButton is being activated
+   */
+  public boolean get() {
+    int dPadValue = m_joystick.getPOV();
+    return (dPadValue == m_direction.direction)
+        || (dPadValue == (m_direction.direction + 45) % 360)
+        || (dPadValue == (m_direction.direction + 315) % 360);
+  }
 }
--- utils/Util.java
+++ utils/Util.java
@@ -1,61 +1,57 @@
 package frc.lib.team7558.utils;
 
-/**
- * Contains some basic utility functions that are used often.
- */
+/** Contains some basic utility functions that are used often. */
 public class Util {
-    /**
-     * Prevent this class from being instantiated.
-     */
-    private Util() {}
+  /** Prevent this class from being instantiated. */
+  private Util() {}
 
+  /**
+   * limit a value to a given range
+   *
+   * @param val - Value to check limits on
+   * @param maxMagnitude - Magnitude to check limits with
+   * @return Value with affected limit
+   */
+  public static double limit(double val, double maxMagnitude) {
+    return limit(val, -maxMagnitude, maxMagnitude);
+  }
 
+  /**
+   * limit a value to a given range
+   *
+   * @param val - Value to check limits on
+   * @param min - Min value to check lower limit with
+   * @param max - Max value to check upper limit with
+   * @return Value with affected limit
+   */
+  public static double limit(double val, double min, double max) {
+    return Math.min(max, Math.max(min, val));
+  }
 
-    /**
-     * limit a value to a given range
-     * 
-     * @param val - Value to check limits on
-     * @param maxMagnitude - Magnitude to check limits with
-     * @return Value with affected limit
-     */
-    public static double limit(double val, double maxMagnitude) {
-        return limit(val, -maxMagnitude, maxMagnitude);
-    }
+  /**
+   * Checks if a value is within a given range
+   *
+   * @param val - Value to check
+   * @param maxMagnitude - Magnitude for range to check
+   * @return If the value is in the given range
+   */
+  public static boolean inRange(double val, double maxMagnitude) {
+    return inRange(val, -maxMagnitude, maxMagnitude);
+  }
 
-    /**
-     * limit a value to a given range
-     * 
-     * @param val - Value to check limits on
-     * @param min - Min value to check lower limit with
-     * @param max - Max value to check upper limit with
-     * @return Value with affected limit
-     */
-    public static double limit(double val, double min, double max) {
-        return Math.min(max, Math.max(min, val));
-    }
+  /**
+   * Checks if a value is within a given range
+   *
+   * @param val - Value to check
+   * @param min - Min value on range
+   * @param max - Max value on range
+   * @return If the value is in the given range
+   */
+  public static boolean inRange(double val, double min, double max) {
+    return val > min && val < max;
+  }
 
-
-
-    /**
-     * Checks if a value is within a given range
-     * 
-     * @param val - Value to check
-     * @param maxMagnitude - Magnitude for range to check
-     * @return If the value is in the given range
-     */
-    public static boolean inRange(double val, double maxMagnitude) {
-        return inRange(val, -maxMagnitude, maxMagnitude);
-    }
-
-    /**
-     * Checks if a value is within a given range
-     * 
-     * @param val - Value to check
-     * @param min - Min value on range
-     * @param max - Max value on range
-     * @return If the value is in the given range
-     */
-    public static boolean inRange(double val, double min, double max) {
-        return val > min && val < max;
-    }
+  public static boolean isCloser(double a, double b, double val) {
+    return Math.abs(a - val) < Math.abs(b - val);
+  }
 }
